8ca80bdadebb2eae397ef0cf629794fbd2d3ecdd
Heidi Wikman <heidi.wikman@sulava.com>
2022-06-09 00:39:26 +0300

    Add project files.

---
+++ W1D4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace W1D4
+{
+    class Program
+    {
+		static void Main(string[] args)
+		{
+			Hello();
+
+			int a = 70;
+			int b = 12;
+			int maxNumber = FindMaxNum(a, b);
+			Console.WriteLine(maxNumber + " is the biggest number");
+		}
+
+		static void Hello()
+		{
+			Console.WriteLine("Hello from a method!");
+		}
+
+
+		static int FindMaxNum(int num1, int num2)
+		{
+			if (num1 > num2)
+				return num1;
+			else
+				return num2;
+		}
+
+
+	}
+}
