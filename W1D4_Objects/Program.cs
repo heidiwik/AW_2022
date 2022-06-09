254750ddc66b12af8cfb4daaf9d48a110088db04
Heidi Wikman <heidi.wikman@sulava.com>
2022-06-09 13:21:25 +0300

    get values from user input

--- W1D4_Objects/Program.cs
+++ W1D4_Objects/Program.cs
@@ -10,12 +10,17 @@ namespace CatExample
         DateTime dateOfBirth;
         string breed;
 
-        public void meow()
+        public static void Welcome()
+        {
+            Console.WriteLine("Welcome to my cat application");
+        }
+
+        public void Meow()
         {
             Console.WriteLine("Meow!");
         }
 
-        static bool inCatShow(string breed)
+        static bool InCatShow(string breed)
         {
             string[] catBreeds = { "Siamese", "Ragdoll", "Persian", "Javanese", "Bengal" };
 
@@ -31,29 +36,41 @@ namespace CatExample
             // lets create an object
             Cat bengal = new Cat();
 
-            // access the method
-            bengal.meow();
+            // access static method
+            Welcome();
+
 
             // use the fields
-            bengal.name = "Bella";
-            bengal.favouriteToy = "Mouse";
+            
+            // ask values from user
+            Console.WriteLine("What is your cat name?");
+            bengal.name = Console.ReadLine();
+
+            Console.WriteLine("What is your cat favourite toy?");
+            bengal.favouriteToy = Console.ReadLine();
+
+            // assign values
             bengal.breed = "Bengal";
             bengal.dateOfBirth = new DateTime(2020, 12, 24);
 
+            // print stuff
             Console.WriteLine("");
-            Console.WriteLine(bengal.name);
-            Console.WriteLine(bengal.favouriteToy);
-            Console.WriteLine(bengal.dateOfBirth.ToString("dd.MM.yyyy"));
+            Console.WriteLine(bengal.name + " says: ");
+            bengal.Meow();
+            Console.WriteLine("\nfavourite toy is: " + bengal.favouriteToy);
+            Console.WriteLine(bengal.name + " is born " + bengal.dateOfBirth.ToString("dd.MM.yyyy"));
 
-            if (!inCatShow("Persian"))
+            // access static method
+            if (InCatShow(bengal.breed))
             {
-                Console.WriteLine("\nPersian cats are not in this cat show");
+                Console.WriteLine(bengal.name + " attending to the cat show!");
             }
 
-            if (inCatShow(bengal.breed))
-            {
-                Console.WriteLine(bengal.name + " is in cat show!");
-            }
+
+            //if (!InCatShow("Persian"))
+            //{
+            //    Console.WriteLine("\nPersian cats are attending in the cat show");
+            //}
 
             Console.ReadLine();
 
