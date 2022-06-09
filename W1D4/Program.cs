88b394eb179070773d155db97a0181046cdde951
Heidi Wikman <heidi.wikman@sulava.com>
2022-06-09 10:56:26 +0300

    method examples

--- W1D4/Program.cs
+++ W1D4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace W1D4
 {
@@ -6,12 +7,20 @@ namespace W1D4
     {
 		static void Main(string[] args)
 		{
-			Hello();
-
 			int a = 70;
 			int b = 12;
-			int maxNumber = FindMaxNum(a, b);
-			Console.WriteLine(maxNumber + " is the biggest number");
+			string maxNumber = FindMaxNum(a, 33);
+
+			string name = "Heidi";
+			int day = 4;
+			SayHello(name, day);
+
+			Console.WriteLine(maxNumber);
+
+
+			string anotherMaxNumber = FindMaxNum(1, 0);
+
+			Console.WriteLine(anotherMaxNumber);
 		}
 
 		static void Hello()
@@ -19,13 +28,29 @@ namespace W1D4
 			Console.WriteLine("Hello from a method!");
 		}
 
-
-		static int FindMaxNum(int num1, int num2)
+		static void SayHello(string username, int daynumber)
 		{
+			Hello();
+			Console.WriteLine("Hello " + username);
+			Console.WriteLine("Today is day " + daynumber + " of the course");
+
+			File.WriteAllText("Userlist.txt", username);
+		}
+
+		static public string FindMaxNum(int num1, int num2)
+		{
+			string result;
+
 			if (num1 > num2)
-				return num1;
+			{
+				result = num1 + " on suurempi numero";
+			}
 			else
-				return num2;
+			{
+				result = num2 + " on suurempi numero";
+			}
+
+			return result;
 		}
 
 
