9b3c7ce10e515c9585cfad75e9f1cfb54366bf72
Heidi Wikman <heidi.wikman@sulava.com>
2022-06-09 11:31:21 +0300

    added static class and line breaks to cat class

--- W1D4_Objects/Program.cs
+++ W1D4_Objects/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace CatExample
 {
@@ -7,10 +8,22 @@ namespace CatExample
         string name;
         string favouriteToy;
         DateTime dateOfBirth;
+        string breed;
 
         public void meow()
         {
             Console.WriteLine("Meow!");
+        }
+
+        static bool inCatShow(string breed)
+        {
+            string[] catBreeds = { "siamese", "ragdoll", "persian", "javanese", "bengal" };
+
+            if (catBreeds.Contains(breed)) {
+                return true;
+            }
+
+            return false;
         }
 
         static void Main(string[] args)
@@ -24,12 +37,18 @@ namespace CatExample
             // use the fields
             bengal.name = "Bella";
             bengal.favouriteToy = "Mouse";
+            bengal.breed = "Bengal";
             bengal.dateOfBirth = new DateTime(2020, 12, 24);
 
             Console.WriteLine("");
             Console.WriteLine(bengal.name);
             Console.WriteLine(bengal.favouriteToy);
             Console.WriteLine(bengal.dateOfBirth.ToString("dd.MM.yyyy"));
+
+            if (!inCatShow("Persian"))
+            {
+                Console.WriteLine("\nPersian cats are not in this cat show");
+            }
 
             Console.ReadLine();
 
