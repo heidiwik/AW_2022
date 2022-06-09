8007f3bcf5638068762a67521d24a0aa4b5de3e1
Heidi Wikman <heidi.wikman@sulava.com>
2022-06-09 10:56:08 +0300

    new examples

---
+++ W1D1_User/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace User
+{
+	class User
+	{
+		// data members
+		int id;
+		string name;
+		int age;
+
+		// constructor
+		User(string name, int age)
+		{
+			this.name = name;
+			this.age = age;
+		}
+
+		// Main Method
+		public static void Main()
+		{
+			User newUser = new User("Ernesti", 21);
+
+			Console.WriteLine(newUser.name);
+			Console.WriteLine(newUser.age);
+		}
+	}
+}
---
+++ W1D4_Objects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CatExample
+{
+    class Cat
+    {
+        string name;
+        string favouriteToy;
+        DateTime dateOfBirth;
+
+        public void meow()
+        {
+            Console.WriteLine("Meow!");
+        }
+
+        static void Main(string[] args)
+        {
+            // lets create an object
+            Cat bengal = new Cat();
+
+            // access the method
+            bengal.meow();
+
+            // use the fields
+            bengal.name = "Bella";
+            bengal.favouriteToy = "Mouse";
+            bengal.dateOfBirth = new DateTime(2020, 12, 24);
+
+            Console.WriteLine("");
+            Console.WriteLine(bengal.name);
+            Console.WriteLine(bengal.favouriteToy);
+            Console.WriteLine(bengal.dateOfBirth.ToString("dd.MM.yyyy"));
+
+            Console.ReadLine();
+
+        }
+    }
+}
