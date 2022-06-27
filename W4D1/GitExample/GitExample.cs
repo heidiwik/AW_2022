b18520aaa1a5d8c7107c2719c5e4f6c0625934ad
Heidi Wikman <heidi.wikman@sulava.com>
2022-06-27 10:21:19 +0300

    Changed user name

---
+++ W4D1/GitExample/GitExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Welcome
+{
+	public class User
+	{
+		int id;
+		string firstName;
+		int age;
+
+		// constructor
+		User(string name, int age)
+		{
+			this.firstName = name;
+			this.age = age;
+		}
+
+		public static void Main()
+		{
+
+			User newUser = new User("Pertti", 72);
+
+			Console.WriteLine("Welcome to course week 4 " + newUser.firstName);
+		}
+
+	}
+}
