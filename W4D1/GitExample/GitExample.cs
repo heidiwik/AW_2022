016c672ff5188d99702207332ecb5c28dd5e4bc4
Heidi Wikman <heidi.wikman@sulava.com>
2022-06-27 10:16:24 +0300

    Added pet to User

---
+++ W4D1/GitExample/GitExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Welcome
+{
+	class Welcome
+    {
+		public static void Main()
+		{
+
+			User newUser = new User("Terttu", "dog");
+
+			Console.WriteLine("Yay you made it to week 4 " + newUser.name + "!");
+			Console.WriteLine("Say hi to your " + newUser.pet);
+		}
+	}
+
+	public class User
+	{
+		int id;
+		public string name;
+		public string pet;
+
+		// constructor
+		public User(string name, string pet)
+		{
+			this.name = name;
+			this.pet = pet;
+
+		}
+
+	}
+}
