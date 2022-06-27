c27fad6ae389a3d355328f07d19797592705fa9d
Heidi Wikman <heidi.wikman@sulava.com>
2022-06-27 09:41:44 +0300

    Added Week 4

---
+++ W4D1/GitExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
