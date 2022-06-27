2c3bbb8f9c35be7d3860f86ee6e69070b8347112
Heidi Wikman <heidi.wikman@sulava.com>
2022-06-27 09:44:17 +0300

    Added git example code

--- W4D1/GitExample/Program.cs
+++ W4D1/GitExample/Program.cs
@@ -2,11 +2,27 @@
 
 namespace GitExample
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class User
+	{
+		// data members
+		int id;
+		string name;
+		int age;
+
+		// constructor
+		User(string name, int age)
+		{
+			this.name = name;
+			this.age = age;
+		}
+
+		// Main Method
+		public static void Main()
+		{
+			User newUser = new User("Ernesti", 21);
+
+			Console.WriteLine(newUser.name);
+			Console.WriteLine(newUser.age);
+		}
+	}
 }
