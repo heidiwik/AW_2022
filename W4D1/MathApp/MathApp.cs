75e26b2050586d3c54f9343db7fa3c8077283760
Heidi Wikman <heidi.wikman@kapsi.fi>
2022-06-28 12:33:40 +0300

    Class library example

---
+++ W4D1/MathApp/MathApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MathLibrary;
+
+namespace MathApp
+{
+    class MathApp
+    {
+        static void Main(string[] args)
+        {
+            MathHelper mathHelper = new MathHelper();
+
+            float tulos = mathHelper.Substract(1340, 3);
+
+            Console.WriteLine("Tulos on: " + tulos);
+
+        }
+    }
+}
---
+++ W4D1/MathLibrary/MathHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class MathHelper
+    {
+        public float Substract(float a, float b)
+        {
+            return a - b;
+        }
+
+        public float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+    }
+}
+    
