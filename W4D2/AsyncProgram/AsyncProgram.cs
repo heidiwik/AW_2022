9e54b41b1d3da7b8b505700ad5c02b8ba5b58486
Heidi Wikman <heidi.wikman@kapsi.fi>
2022-06-28 16:20:06 +0300

    Async example

---
+++ W4D2/AsyncProgram/AsyncProgram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AsyncProgram
+{
+    class AsyncProgram
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+
+            Task.WaitAll(first(), second(), third());
+
+            sw.Stop();
+
+            var elapsed = sw.ElapsedMilliseconds;
+            Console.WriteLine($"elapsed: {elapsed} ms");
+
+
+            async Task first()
+            {
+                await Task.Delay(3000);
+                Console.WriteLine("first finished");
+            }
+
+            async Task second()
+            {
+                await Task.Delay(6000);
+                Console.WriteLine("second finished");
+            }
+
+            async Task third()
+            {
+                await Task.Delay(1000);
+                Console.WriteLine("third finished");
+            }
+        }
+    }
+}
