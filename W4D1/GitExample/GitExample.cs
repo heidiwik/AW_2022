4065e8b7a8d9c7e373665378d207a581d5aea053
Heidi Wikman <heidi.wikman@kapsi.fi>
2022-06-29 10:04:13 +0300

    Added W1D3 exercise code

--- W4D1/GitExample/GitExample.cs
+++ W4D1/GitExample/GitExample.cs
@@ -2,30 +2,30 @@
 
 namespace Welcome
 {
-	class Welcome
+    class Welcome
     {
+        public static void Main()
+        {
+            User newUser = new User("Terttu", "dog");
 
-			User newUser = new User("Terttu", "dog");
+            Console.WriteLine("Yay you made it to week 4 " + newUser.name + "!");
+            Console.WriteLine("Say hi to your " + newUser.pet);
+        }
 
-			Console.WriteLine("Yay you made it to week 4 " + newUser.name + "!");
-			Console.WriteLine("Say hi to your " + newUser.pet);
-		}
-	}
+    }
+}
 
-	public class User
-	{
-		int id;
-		public string name;
-		public string pet;
+public class User
+{
+    int id;
+    public string name;
+    public string pet;
 
-		// constructor
-		public User(string name, string pet)
-		{
-			this.name = name;
-			this.pet = pet;
+    // constructor
+    public User(string name, string pet)
+    {
+        this.name = name;
+        this.pet = pet;
+    }
 
-			Console.WriteLine("Welcome to course week 4 " + newUser.firstName);
-		}
-
-	}
 }
---
+++ W4D3/BrokenLottoGame/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Threading;
+
+/* 
+ * Exercise: 
+ * Recognise parts that can throw an error
+ * Modify the code and add error handling where needed  
+ */
+
+
+namespace BrokenLottoGame
+{
+    public class Raffle
+    {
+        public bool victory;
+        public int guessedNumber;
+        public int victoryNumber;
+        public string[] price = new string[]
+        {
+            "Pony",
+            "One million dollars",
+            "Bike",
+            "Hat",
+            "Ice cream"
+        };
+
+        public void Welcome()
+        {
+            Console.WriteLine("\n\n*********************************");
+            Console.WriteLine("Welcome to the lotto game\n");
+        }
+
+        static void ShowDots()
+        {
+            int i = 0;
+            while (i < 3)
+            {
+                Console.Write(".");
+                Thread.Sleep(1000);
+                i++;
+            }
+            Console.WriteLine("\n\n");
+        }
+
+        void GetResult()
+        {
+            var rand = new Random();
+            victoryNumber = rand.Next(10);
+
+            if (victoryNumber == guessedNumber)
+            {
+                victory = true;
+            }
+            else
+            {
+                victory = false;
+            }
+        }
+
+        public void Play()
+        {
+            Console.WriteLine("Guess a number between 1-10:\n");
+            guessedNumber = int.Parse(Console.ReadLine());
+            Console.Write("\nGood luck!\n\nRaffling ");
+            ShowDots();
+            GetResult();
+        }
+    }
+
+
+
+    class Exercise
+    {
+        static void Main(string[] args)
+        {
+            Raffle lotto = new Raffle();
+
+            lotto.Welcome();
+            lotto.Play();
+
+            if (lotto.victory)
+            {
+                Console.Write(" Congratulations! You won ");
+                Console.Write(lotto.price[lotto.victoryNumber]);
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine(" Correct number was " + lotto.victoryNumber + "\n You lost :(  No price for you\n");
+            }
+
+            Console.WriteLine("*********************************");
+        }
+    }
+}
