679b43baa1966b50837bd3afd5f65880c6863750
Heidi Wikman <heidi.wikman@kapsi.fi>
2022-06-30 09:49:31 +0300

    Exception example

---
+++ W4D3/ExceptionExample/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+
+namespace ExceptionExample
+{
+    class ExceptionExample
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // string[] text = File.ReadAllLines("/../../teksti.txt");
+
+                int[] number = { 1, 2, 3, 7, 21, 4 };
+
+                Console.WriteLine(number[4]);
+                Console.WriteLine(number[8]);
+            }
+            catch(FileNotFoundException e)
+            {
+                Console.WriteLine("\nTiedostoa ei löytynyt\n");
+                Console.WriteLine(e.ToString());
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("\nVirhe: IndexOutOfRangeException\n");
+                Console.WriteLine(e.ToString());
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("\nJoku muu IO virhe\n");
+                Console.WriteLine(e.Message);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("\nJoku ihan muu virhe\n");
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Hello o/");
+        }
+    }
+}
