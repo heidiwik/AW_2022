0e2740e6aa47afbf7b69a7c6bc4cde7375a8896d
Heidi Wikman <heidi.wikman@kapsi.fi>
2022-07-27 13:54:01 +0300

    JSON example

--- W7D3/local-storage-example/js/script.js
+++ W7D3/local-storage-example/js/script.js
@@ -12,4 +12,30 @@ function changeTextColor(color) {
     localStorage.setItem("selectedColor", color);
     const elem = document.getElementById('title');
     elem.style.color = color;
+} 
+
+
+// JSON stringify example
+
+const userSelection = {
+  color: "#ff000",
+  language: "FI",
+  news: 5,
+  showAds: false
 }
+
+// convert object to JSON string
+const userSelectionString = JSON.stringify(userSelection);
+console.log(userSelectionString);
+
+// save to localStorage
+localStorage.setItem("userSelectionObject", userSelectionString);
+
+
+// get from localStorage
+var selection = localStorage.getItem("userSelectionObject");
+
+// parse back to object
+var selectionObject = JSON.parse(selection);
+
+console.log(selectionObject.showAds);
