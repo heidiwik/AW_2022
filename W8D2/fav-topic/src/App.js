f29b46e5abf40f563893f0d55272c414d6121240
Heidi Wikman <heidi.wikman@kapsi.fi>
2022-08-02 17:53:39 +0300

    React state example

---
+++ W8D2/fav-topic/src/App.js
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [color, setSelection] = useState("ei mikään :(");
+
+  return (
+    <>
+      <h2>Lempiaiheeni kurssilla on: {color}</h2>
+
+      <p></p>
+      <p>Valitse lempiaihe:</p>
+      <button type="button" onClick={() => setSelection("C#")}>C#</button>
+      <button type="button" onClick={() => setSelection("React")}>React</button>
+      <button type="button" onClick={() => setSelection("HTML & CSS")}>HTML & CSS</button>
+      <button type="button" onClick={() => setSelection("JavaScript")}>JavaScript</button>
+    </>
+  )
+};
+ 
+export default App
